249f4848b5e4e26ea71f91bd11d1d9da1cfac15a
Izhar Ul Haq <intern.seo@itkumail.com>
2026-06-22 19:08:57 +0500

    feat: disable index sitemap generation and add static sitemap.xml file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,6 +5,7 @@ module.exports = {
   // If you want next-sitemap to generate robots.txt, set this to true
   // and remove public/robots.txt from the repo.
   generateRobotsTxt: false,
+  generateIndexSitemap: false,
   changefreq: "weekly",
   priority: 0.8,
   exclude: [
